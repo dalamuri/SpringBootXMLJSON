1860a477ce7944ec75c2541f607fe179df1d76a6
sougandh <dalamuri09@gmail.com>
2018-04-06 15:01:11 -0500

    xmlsecondcommit

---
+++ src/main/java/com/sou/xml/RestHomeController.java
@@ -0,0 +1,50 @@
+package com.sou.xml;
+
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestHomeController {
+	
+	@GetMapping(value="/home",produces= {"application/xml","application/json"})
+	public @ResponseBody Student getStudent()
+	{
+		
+		Student student=new Student();
+		student.setFirstName("sougandh");
+		student.setLastName("kumar");
+		student.setUsername("soug");
+		return student;
+	}
+	@XmlRootElement
+	class  Student{
+		String Username;
+		String FirstName;
+		String LastName;
+		public String getUsername() {
+			return Username;
+		}
+		public void setUsername(String username) {
+			Username = username;
+		}
+		public String getFirstName() {
+			return FirstName;
+		}
+		public void setFirstName(String firstName) {
+			FirstName = firstName;
+		}
+		public String getLastName() {
+			return LastName;
+		}
+		public void setLastName(String lastName) {
+			LastName = lastName;
+		}
+		
+	}
+	
+
+}
---
+++ src/main/java/com/sou/xml/SpringBootXmljssonApplication.java
@@ -0,0 +1,13 @@
+package com.sou.xml;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootXmljssonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootXmljssonApplication.class, args);
+	}
+}
